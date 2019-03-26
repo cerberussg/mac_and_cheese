25adb397d6130f1ab4a03cd6870eace08ecef926
root <root@localhost.localdomain>
2019-03-26 15:45:35 -0400

    implemented ability to change mac address in linux/macos

--- mac_and_cheese.py
+++ mac_and_cheese.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 
+import subprocess
+
+subprocess.call("ifconfig eth0 down", shell=True)
+print("Bringing network eth0 down")
+subprocess.call("ifconfig eth0 hw ether 08:00:27:F8:42:A7", shell=True)
+print("Changing MAC address on eth0 to: 08:00:27:F8:42:A7")
+subprocess.call("ifconfig eth0 up", shell=True)
+print("Bringing network eth0 up")

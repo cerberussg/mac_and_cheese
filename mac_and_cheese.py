993e0515360e734611af25a9a8e3bacc7da5c7f4
Scott Goyette <scott.goyette@rubypythmore.com>
2019-03-29 17:30:50 -0400

    updated code, generate random MAC address

--- mac_and_cheese.py
+++ mac_and_cheese.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 
 import subprocess
+import random
 from pyfiglet import Figlet
 
+
+def rand_mac():
+    return "%02x:%02x:%02x:%02x:%02x:%02x" % (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255)
+        )
+
+
 custom_font = Figlet(font='doom')
+interface = "en6"
+new_mac = rand_mac()
 
 print(custom_font.renderText("Mac & Cheese"))
-
-subprocess.call("ifconfig eth0 down", shell=True)
-print("Bringing network eth0 down")
-subprocess.call("ifconfig eth0 hw ether 08:00:27:F8:42:A7", shell=True)
-print("Changing MAC address on eth0 to: 08:00:27:F8:42:A7")
-subprocess.call("ifconfig eth0 up", shell=True)
-print("Bringing network eth0 up")
+print("[+] Changing MAC address for " + interface)
+# subprocess.call("sudo ifconfig en7 down", shell=True)
+# print("Bringing network en7 down")
+subprocess.call("sudo ifconfig " + interface + " ether " + new_mac, shell=True)
+print("[+] Assigning MAC address: " + new_mac)
+subprocess.call("sudo ifconfig " + interface + " up", shell=True)
+print("[+] Bringing network " + interface + " up")

e85ddea8fabec9b44a40d580d6af03be92c17d94
Scott Goyette <scott.goyette@rubypythmore.com>
2019-04-04 14:34:20 -0400

    options now work and can assign form command line, default mac is to randomize address.

--- mac_and_cheese.py
+++ mac_and_cheese.py
@@ -8,8 +8,10 @@ from pyfiglet import Figlet
 
 parser = ArgumentParser(description='MAC spoofer written in Python')
 parser.add_argument('-i', '--interface', dest='interface', help='Interface adapter name to change MAC address on.')
-parser.add_argument('-m', '--mac_address', dest='new_mac', help='Assign a MAC address to interface instead of a random one.')
+parser.add_argument('-m', '--mac', dest='new_mac', help='Assign a MAC address to interface instead of a random one.')
 args = parser.parse_args()
+custom_font = Figlet(font='doom')
+print(custom_font.renderText("Mac & Cheese"))
 
 
 def rand_mac():
@@ -23,22 +25,22 @@ def rand_mac():
         )
 
 
-def darwin():
+def darwin(face, mac):
+    subprocess.call(["sudo", "ifconfig", face, "ether", mac])
+    subprocess.call(["sudo", "ifconfig", face, "up"])
+    print(f'[+] Changing MAC address for {face}')
+    print(f'[+] Assigning MAC address: {mac}')
+    print(f'[+] Bringing network {face} up')
+
+
+interface = args.interface
+new_mac = args.new_mac
+if new_mac is None:
     new_mac = rand_mac()
-    interface = input("Enter interface you wish to change > ")
-    subprocess.call(["sudo", "ifconfig", interface, "ether", new_mac])
-    print(f'[+] Changing MAC address for {interface}')
-    print(f'[+] Assigning MAC address: {new_mac}')
-    print(f'[+] Bringing network {interface} up')
-    subprocess.call(["sudo", "ifconfig", interface, "up"])
 
-
-custom_font = Figlet(font='doom')
-print(custom_font.renderText("Mac & Cheese"))
-# print(sys.platform.startswith('darwin'))
 
 if sys.platform.startswith('darwin'):
-    darwin()
+    darwin(interface, new_mac)
 elif sys.platform.startswith('linux'):
     # TODO: linux code
     pass
